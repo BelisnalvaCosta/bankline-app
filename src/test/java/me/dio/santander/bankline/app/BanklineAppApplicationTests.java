1efcf26a16ed51f6c45c4186caec857ee454241d
https://github.com/BelisnalvaCosta/bankline-app.git <bella-costa@hotmail.com.br>
2024-01-11 13:15:33 -0300

    Committing packages and files

---
+++ src/test/java/me/dio/santander/bankline/app/BanklineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander.bankline.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanklineAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
